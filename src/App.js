ce855859d9a75248daf71cb05b1caf8a5519d5a9
serteri <serteri@hotmail.com>
2021-12-01 22:23:26 +1000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from './Home';
+import Movie from './Movie';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+const NotFound = () => {
+  return <h2>404 Not Found</h2>;
+}
+
+const App = () => {
+  return (
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">Movie Finder 2</Link>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/movie/:id" component={Movie} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
+export default App;
